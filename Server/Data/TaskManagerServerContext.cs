34d5fdb89b799797468c2c9570acaafb9b603d33
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-03-06 22:32:37 -0500

    Add project files.

---
+++ Server/Data/TaskManagerServerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Shared.Models;
+
+namespace TaskManager.Server.Data
+{
+    public class TaskManagerServerContext : DbContext
+    {
+        public TaskManagerServerContext (DbContextOptions<TaskManagerServerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskManager.Shared.Models.TaskItem> TaskItem { get; set; }
+    }
+}
